281d2fbeef16e45de7926d3c994a712e09fcf755
Cornee van Blerk <corneevanblerk@gmail.com>
2025-01-29 11:39:24 +0100

    2024: day 2

--- 2024/day_2/solve.py
+++ 2024/day_2/solve.py
@@ -4,49 +4,19 @@ def parse_input():
     with open(0) as f:
         return [line.strip() for line in f.readlines()]
 
-def is_sorted(lst):
-    return all(lst[i] <= lst[i + 1] for i in range(len(lst) - 1)) or \
-           all(lst[i] >= lst[i + 1] for i in range(len(lst) - 1))
+def solve_p1(report):
+    return all(1 <= abs(n1 - n2) <= 3 for n1, n2 in zip(report, report[1:])) and \
+           (sorted(report) == report or sorted(report, reverse=True) == report)
 
-def solve_p1(reports):
-    reports = [report for report in reports if is_sorted(report)]
-    total = 0
-    for report in reports:
-        valid = True
-        for i in range(0, len(report) - 1):
-            distance = abs(report[i] - report[i + 1])
-            if distance < 1 or distance > 3:
-                valid = False
-                break
-        if valid:
-            total += 1
-    return total
-
-def solve_p2(reports):
-    reports = [report for report in reports if is_sorted(report)]
-    total = 0
-    for report in reports:
-        valid = True
-        toleration_used = False
-        for i in range(0, len(report) - 1):
-            distance = abs(report[i] - report[i + 1])
-            if distance < 1 or distance > 3:
-                if not toleration_used:
-                    toleration_used = True
-                else:
-                    valid = False
-                    break
-        if valid:
-            total += 1
-    return total
+def solve_p2(report):
+    return any(solve_p1(report[:i] + report[i + 1:]) for i in range(len(report)))
 
 lines = parse_input()
 
 reports = [[int(num) for num in re.findall(r'\d+', line)] for line in lines]
 
-p1 = solve_p1(reports)
+p1 = sum(solve_p1(report) for report in reports)
 print("Part 1: %d" % p1)
 
-p2 = solve_p2(reports)
+p2 = sum(solve_p2(report) for report in reports)
 print("Part 2: %d" % p2)
-
