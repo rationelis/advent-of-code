c3765117ceca860a7df330df628e89e78bd1925c
Cornee van Blerk <corneevanblerk@gmail.com>
2025-03-15 17:39:33 +0100

    2024: finish day 9

--- 2024/day_9/solve.py
+++ 2024/day_9/solve.py
@@ -1,6 +1,9 @@
 def print_solution(array):
     print("".join(str(e) if e != -1 else "." for e in array))
 
+def calc_checksum(array):
+    return sum(index * value for index, value in enumerate(array) if (value != -1))
+
 with open(0) as f:
     line = f.readlines()[0].strip()
 
@@ -11,18 +14,50 @@ for num in map(int, line):
     id += not alloc_space
     alloc_space = not alloc_space
 
-print_solution(array)
+def solve_p1(array):
+    left = 0
+    for index in range(len(array) - 1, -1, -1):
+        if left >= index:
+            break
+        if array[index] == -1:
+            continue
+        while left < len(array) and array[left] != -1:
+            left += 1
+        if left < index:
+            array[left], array[index] = array[index], -1
+    return calc_checksum(array)
 
-for (index, elem) in reversed(list(enumerate(array))):
-    if elem != -1:
-        # We swap with first 
-        swap_index = 0
-        for (seq_index, seq_elem) in enumerate(array):
-            if seq_elem == -1:
-                swap_index = seq_index
-                break
-        array[swap_index] = elem
-        array[index] = -1
+def solve_p2(array):
+    file_positions = {}
+    i = 0
+    while i < len(array):
+        if array[i] != -1:
+            file_id = array[i]
+            start = i
+            while i < len(array) and array[i] == file_id:
+                i += 1
+            file_positions[file_id] = (start, i - start)
+        else:
+            i += 1
+    for file_id in sorted(file_positions.keys(), reverse=True):
+        start, size = file_positions[file_id]
+        free_start = None
+        free_length = 0
+        for i in range(len(array)):
+            if array[i] == -1:
+                if free_length == 0:
+                    free_start = i
+                free_length += 1
+                if free_length == size:
+                    break
+            else:
+                free_start = None
+                free_length = 0
+        if free_start is not None and free_start < start:
+            for j in range(size):
+                array[free_start + j] = file_id
+                array[start + j] = -1
+    return calc_checksum(array)
 
-print_solution(array)
-
+print(f"Part 1: {solve_p1(array[:])}")
+print(f"Part 2: {solve_p2(array[:])}")
